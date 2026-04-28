b2d707aaabbde2ed6fc21556daba6a5c409eeaa1
snipe <snipe@snipe.net>
2026-04-28 16:25:55 +0100

    Pint

--- app/Presenters/CustomFieldPresenter.php
+++ app/Presenters/CustomFieldPresenter.php
@@ -26,26 +26,27 @@ final class CustomFieldPresenter extends Presenter
     {
         $icons = [];
         if ($this->field->display_checkout) {
-            $icons[] = '<span title="' . e(trans('admin/custom_fields/general.display_checkout')) . '" data-tooltip="true"><i class="fa-solid fa-rotate-left text-muted"></i></span>';
+            $icons[] = '<span title="'.e(trans('admin/custom_fields/general.display_checkout')).'" data-tooltip="true"><i class="fa-solid fa-rotate-left text-muted"></i></span>';
         }
         if ($this->field->display_checkin) {
-            $icons[] = '<span title="' . e(trans('admin/custom_fields/general.display_checkin')) . '" data-tooltip="true"><i class="fa-solid fa-rotate-right text-muted"></i></span>';
+            $icons[] = '<span title="'.e(trans('admin/custom_fields/general.display_checkin')).'" data-tooltip="true"><i class="fa-solid fa-rotate-right text-muted"></i></span>';
         }
         if ($this->field->display_audit) {
-            $icons[] = '<span title="' . e(trans('admin/custom_fields/general.display_audit')) . '" data-tooltip="true"><i class="fas fa-clipboard-check  text-muted"></i></span>';
+            $icons[] = '<span title="'.e(trans('admin/custom_fields/general.display_audit')).'" data-tooltip="true"><i class="fas fa-clipboard-check  text-muted"></i></span>';
         }
         if ($this->field->display_in_user_view) {
-            $icons[] = '<span title="' . e(trans('admin/custom_fields/general.display_in_user_view_table')) . '" data-tooltip="true"><i class="fas fa-user text-muted"></i></span>';
+            $icons[] = '<span title="'.e(trans('admin/custom_fields/general.display_in_user_view_table')).'" data-tooltip="true"><i class="fas fa-user text-muted"></i></span>';
         }
         if ($this->field->show_in_listview) {
-            $icons[] = '<span title="' . e(trans('admin/custom_fields/general.show_in_listview_short')) . '" data-tooltip="true"><i class="fas fa-list text-muted"></i></span>';
+            $icons[] = '<span title="'.e(trans('admin/custom_fields/general.show_in_listview_short')).'" data-tooltip="true"><i class="fas fa-list text-muted"></i></span>';
         }
         if ($this->field->show_in_email) {
-            $icons[] = '<span title="' . e(trans('admin/custom_fields/general.show_in_email_short')) . '" data-tooltip="true"><i class="fas fa-envelope text-muted"></i></span>';
+            $icons[] = '<span title="'.e(trans('admin/custom_fields/general.show_in_email_short')).'" data-tooltip="true"><i class="fas fa-envelope text-muted"></i></span>';
         }
         if ($this->field->show_in_requestable_list) {
-            $icons[] = '<span title="' . e(trans('admin/custom_fields/general.show_in_requestable_list_short')) . '" data-tooltip="true"><i class="fa-solid fa-bell-concierge text-muted"></i></span>';
+            $icons[] = '<span title="'.e(trans('admin/custom_fields/general.show_in_requestable_list_short')).'" data-tooltip="true"><i class="fa-solid fa-bell-concierge text-muted"></i></span>';
         }
+
         return $icons;
     }
 
