cc0169d2f7221a84c348dbde0d3e8195996d9111
snipe <snipe@snipe.net>
2026-05-07 16:01:13 +0100

    Use auth:api on routes

--- routes/ai.php
+++ routes/ai.php
@@ -4,4 +4,4 @@ use App\Mcp\Servers\SnipeMCPServer;
 use Laravel\Mcp\Facades\Mcp;
 
 Mcp::oauthRoutes();
-Mcp::web('/mcp/snipe-it', SnipeMCPServer::class);
+Mcp::web('/mcp/snipe-it', SnipeMCPServer::class)->middleware('auth:api');
